e3c65dec2f10a871f5a9a606fd5df54868b8372c
Zack Emerson <zacme429@gmail.com>
2020-04-17 17:00:19 -0600

    Jumbotron has title and subtitle

---
+++ clickygame/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbtron">
+            <div className="container text-center">
+                <h1 className="display-1">Clicky Memory Game</h1>
+                <p classname="subtitle">Click on the images but don't click on the same image twice</p>
+            </div>
+        </div> 
+  );
+}
+
+export default Jumbotron;
