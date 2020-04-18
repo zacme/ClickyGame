0a75c86b9a16d71322dc4ffc7b828b560a0311dc
Zack Emerson <zacme429@gmail.com>
2020-04-17 22:06:59 -0600

    fixed spacing on the navbar

--- clickygame/src/components/Navbar/Navbar.js
+++ clickygame/src/components/Navbar/Navbar.js
@@ -2,13 +2,22 @@ import React from "react";
 import "./Navbar.css";
 
 const Nav = (props) => (
-    <nav classname= "navbar" expand="lg" bg="dark">
-        <a className="navbar-brand" href="/">
-            Clicky Game
-        </a>
-        <h4 className="nav-message">{props.message}</h4>
-        <h4 className="nav-score">Current: {props.currentScore} | Top: {props.topScore}</h4>
-    </nav>
+    <div classname="navbar bg-dark">
+        <div className="row">
+            <div className="col-md-4">
+                <a className="navbar-brand" href="/">
+                    Clicky Game
+                </a>
+            </div>
+            <div classname="col-md-4">
+                <h4 className="nav-message">{props.message}</h4>
+            </div>
+            <div className="col-md-4">
+                <h4 className="nav-score">Current: {props.currentScore} | Top: {props.topScore}</h4>
+
+            </div>
+        </div>
+        </div>
 );
 
 export default Nav;
