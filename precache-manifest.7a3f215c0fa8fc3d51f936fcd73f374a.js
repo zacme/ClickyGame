750cd83af334f7ad0db21fbcc39951d611f0cc0e
Zack Emerson <zacme429@gmail.com>
2020-05-06 12:51:41 -0600

    Updates

---
+++ precache-manifest.7a3f215c0fa8fc3d51f936fcd73f374a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f91774f4475a5cf0b126b6f5af4930",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "1a1fad071aae66378241",
+    "url": "/ClickyGame/static/css/main.bcbe7034.chunk.css"
+  },
+  {
+    "revision": "9e5142133b7c737dccae",
+    "url": "/ClickyGame/static/js/2.213308e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.213308e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1fad071aae66378241",
+    "url": "/ClickyGame/static/js/main.cc241500.chunk.js"
+  },
+  {
+    "revision": "0d4bd3bd13acb97fa8f2",
+    "url": "/ClickyGame/static/js/runtime-main.1d742c2c.js"
+  }
+]);
